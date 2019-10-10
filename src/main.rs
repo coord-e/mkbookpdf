4b1cda6dc4c0b22af711f38916933ddb6acc299d
coord.e <me@coord-e.com>
2019-10-10 09:03:15 +0000

    Add: Add minimal cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use lopdf::{Document, Error};
+use std::path::PathBuf;
+use lopdf::{Document, Error, Object};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "mkbooklet")]
+struct Opt {
+    #[structopt(short, long, parse(from_os_str))]
+    output: PathBuf,
+
+    #[structopt(name = "FILE", parse(from_os_str))]
+    input: PathBuf,
+}
 
 fn main() -> Result<(), Error> {
-    let mut doc = Document::load("example.pdf")?;
-    doc.version = "1.4".to_string();
-    doc.replace_text(1, "Hello World!", "Modified text!")?;
-    doc.save("modified.pdf")?;
+    let opt = Opt::from_args();
+    let mut doc = Document::load(opt.input)?;
+    doc.replace_text(1, "a", "horaaaaaaay!")?;
+    doc.save(opt.output)?;
     Ok(())
 }
