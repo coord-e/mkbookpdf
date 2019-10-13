bc303ddcd8a009d3de5e0b124ed846fdea2d4e73
coord.e <me@coord-e.com>
2019-10-13 07:21:01 +0000

    Add: Add a test cases of print feature

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,8 @@
 use assert_cmd::crate_name;
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
+use assert_fs::NamedTempFile;
+use predicates::prelude::*;
 use std::process::Command;
 
 #[test]
@@ -17,4 +20,34 @@ fn test_no_output() {
         .arg("tests/data/sample.pdf")
         .assert()
         .failure();
+}
+
+#[test]
+fn test_print() {
+    Command::cargo_bin(crate_name!())
+        .unwrap()
+        .env("MKBL_LP", "echo")
+        .arg("tests/data/sample.pdf")
+        .arg("--print")
+        .assert()
+        .stdout(predicate::str::contains("-o sides=two-sided-short-edge"))
+        .stdout(predicate::str::contains("-o number-up=2"))
+        .stdout(predicate::str::contains("-o number-up-layout=lrtb"));
+}
+
+#[test]
+fn test_print_output() {
+    let temp = NamedTempFile::new("tmp.pdf").unwrap();
+    let path = temp.path();
+
+    Command::cargo_bin(crate_name!())
+        .unwrap()
+        .env("MKBL_LP", "echo")
+        .arg("tests/data/sample.pdf")
+        .args(&["-o", path.to_str().unwrap()])
+        .arg("--print")
+        .assert()
+        .stdout(predicate::str::contains(path.to_str().unwrap()));
+
+    temp.assert(predicate::function(|x: &[u8]| !x.is_empty()).from_file_path());
 }
